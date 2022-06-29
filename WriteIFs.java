b34ab218fe05a2fda1b7689cb13f2bcbacb63c67
Nick Nilson <nilson.nicholas@gmail.com>
2022-06-29 18:25:41 -0400

    WriteIFs written & tested

--- WriteIFs.java
+++ WriteIFs.java
@@ -11,6 +11,8 @@ public class WriteIFs
     public void playerDied(boolean player1) {
         // Write an IF statement that checks “player1.isAlive()” 
         // and if that’s false, calls “displayGameOver(player1)”
+     displayGameOver(player1);
+         
      
     }
     
@@ -18,9 +20,11 @@ public class WriteIFs
         // Write an IF statement that checks the 
         // “temperature(room)” and if that check is less than 70, 
         // calls “heatOn()” else calls “coolOn()”
-
-
-        
+        if (tempurature(room) < 70) {
+            heatOn();
+        } else {
+            coolOn();
+        }
         return this.ss;
     }
 
@@ -30,13 +34,17 @@ public class WriteIFs
         // AND 
         // “insideTemp()” is less than 62, 
         // calls “startAFire(fireplace1)”
-
+        if (outsideTemp() < 50 && insideTemp() < 62) {
+            startAFire(fireplace1);
+        }
     }
 
     public void checkFuel(double fuelLevel) {
         // Write an IF statement that checks “fuelLevel” 
         // and if that check is less than 0.08, calls “refuel()”
-
+    if (fuelLevel < 0.08) {
+        refuel();
+    }
     }
 
 
@@ -57,20 +65,21 @@ public class WriteIFs
   /**
    * Constructor for objects of class WriteIFs
    */
-  public WriteIFs()
-  {
+  //public WriteIFs(){
+  
       // initialise instance variables
-      x = 0;
-      tt_t = 0;
-      tt_s = 1;
-      ss = "";
-      oo1 = 61;
-      oo2 = 49;
-  }
+      int x = 0;
+      int tt_t = 0;
+      int tt_s = 1;
+      String ss = "";
+      int oo1 = 61;
+      int oo2 = 49;
+    
+  
 
     // associated routines
     public boolean isAlive(boolean p) {
-        return !p;
+        return p;
     }
     private int tempurature(int t) {
         return t+2;
